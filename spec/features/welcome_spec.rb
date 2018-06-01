f0bb95f85ddd2952e4b9fe35ef254b5d103f84ad
Tom Betts <tom.betts@live.co.uk>
2018-06-01 12:58:08 +0100

    Pass welcome message test and fix rubocop offenses.

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.feature 'Homepage' do
-  scenario 'Can visit homepage and see welcome message' do
-    visit('/')
-    expect(page).to have_content('Welcome to Makers Instagram')
+RSpec.feature "Homepage" do
+  scenario "Can visit homepage and see welcome message" do
+    visit("/")
+    expect(page).to have_content("Welcome to Makers Instagram")
   end
 end
