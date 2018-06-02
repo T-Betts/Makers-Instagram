c521759260c69364ed4be1fece49fd5ba2d57c40
Tom Betts <tom.betts@live.co.uk>
2018-06-02 10:56:09 +0100

    Update welcome message.

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -5,6 +5,6 @@ require "rails_helper"
 RSpec.feature "Homepage" do
   scenario "Can visit homepage and see welcome message" do
     visit("/")
-    expect(page).to have_content("Welcome to Makers Instagram")
+    expect(page).to have_content("Welcome to Picture Rails")
   end
 end
