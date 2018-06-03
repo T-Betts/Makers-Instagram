b23fcde6689d7ca47210675f8cb90016601686cd
Tom Betts <tom.betts@live.co.uk>
2018-06-03 13:08:19 +0100

    Write test for users seeing their profile page after logging in.

--- spec/features/profile_page_spec.rb
+++ spec/features/profile_page_spec.rb
@@ -1,11 +1,11 @@
 # # frozen_string_literal: true
-#
-# require "rails_helper"
-#
-# RSpec.feature "Profile Page" do
-#   scenario "After logging in I am taken to my profile page" do
-#     visit("/")
-#     sign_up
-#     expect(page).to have_content("")
-#   end
-# end
+
+require "rails_helper"
+
+RSpec.feature "Profile Page" do
+  scenario "After logging in, users see their profile page" do
+    visit("/")
+    sign_up
+    expect(page).to have_content("Tom123 Profile Page")
+  end
+end
