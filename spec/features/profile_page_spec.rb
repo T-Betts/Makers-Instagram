f06f180e738b320da5dffcd27422725138ec8527
Tom Betts <tom.betts@live.co.uk>
2018-06-03 13:22:07 +0100

    Render user profile text when logged in.

--- spec/features/profile_page_spec.rb
+++ spec/features/profile_page_spec.rb
@@ -6,6 +6,6 @@ RSpec.feature "Profile Page" do
   scenario "After logging in, users see their profile page" do
     visit("/")
     sign_up
-    expect(page).to have_content("Tom123 Profile Page")
+    expect(page).to have_content("Tom123's Profile Page")
   end
 end
