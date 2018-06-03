1c2900d421b0bb4ee4540f2de2fc667cfd9ff07b
Tom Betts <tom.betts@live.co.uk>
2018-06-03 19:48:01 +0100

    Remove 'picture' controller files. Generate 'pictures' controller.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get "welcome/index"
   get "picture/index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :pictures
+  end
   root "welcome#index"
 end
