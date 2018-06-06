3520528e1bffc8ffc60a6d5bbacab11cd90bdb96
Tom Betts <tom.betts@live.co.uk>
2018-06-06 15:13:49 +0100

    Write test for delete picture functionality.

--- spec/features/picture_post_spec.rb
+++ spec/features/picture_post_spec.rb
@@ -2,8 +2,8 @@
 
 require "rails_helper"
 
-RSpec.feature "Profile Page" do
-  scenario "Uploads a picture" do
+RSpec.feature "Pictures" do
+  scenario "Can upload pictures." do
     sign_up
     click_button "Profile"
     click_button "Upload Picture"
@@ -11,5 +11,16 @@ RSpec.feature "Profile Page" do
     click_button "Post"
     click_button "Profile"
     expect(page).to have_css("img[src*='another_cat.jpeg']")
+  end
+
+  scenario "Can delete pictures." do
+    sign_up
+    click_button "Profile"
+    click_button "Upload Picture"
+    page.attach_file("picture_image", Rails.root + "app/assets/images/another_cat.jpeg")
+    click_button "Post"
+    click_button "Profile"
+    click_button "Delete"
+    expect(page).not_to have_css("img[src*='another_cat.jpeg']")
   end
 end
