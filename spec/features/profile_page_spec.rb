32e37113f0a93d4e81da3b0c66b5d3bd2dea1317
Tom Betts <tom.betts@live.co.uk>
2018-06-06 13:11:01 +0100

    Write test for 'All Users' functionality

--- spec/features/profile_page_spec.rb
+++ spec/features/profile_page_spec.rb
@@ -4,7 +4,6 @@ require "rails_helper"
 
 RSpec.feature "Profile Page" do
   scenario "After logging in, users can click a button and see their profile page" do
-    visit("/")
     sign_up
     click_button "Profile"
     expect(page).to have_content("Tom123's Profile Page")
