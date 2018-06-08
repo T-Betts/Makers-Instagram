cbce46254b7c2fe70de99f81e79ddd31c6243b7d
Tom Betts <tom.betts@live.co.uk>
2018-06-08 11:14:23 +0100

    Write feature test for commenting on picture.

---
+++ spec/features/picture_comment_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.feature "Picture Comments" do
+  scenario "Can comment on a picture." do
+    sign_up
+    click_button "Profile"
+    click_button "Upload Picture"
+    page.attach_file("picture_image", Rails.root + "app/assets/images/another_cat.jpeg")
+    click_button "Post"
+    click_button "Comment"
+    fill_in "Comment", with: "Nice Picture!"
+    click_button "Comment"
+    expect(page).to have_content("Nice Picture!")
+  end
+end
