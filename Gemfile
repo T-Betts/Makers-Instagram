ccd124c87a1a84fa654e63106dfe8576032f5570
Tom Betts <tom.betts@live.co.uk>
2018-06-08 09:33:59 +0100

    Update picture specs accordingly.

--- Gemfile
+++ Gemfile
@@ -4,7 +4,6 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby "2.5.0"
 
 gem "bootstrap"
-gem "bootstrap-sass"
 gem "jquery-rails"
 gem "devise"
 gem "dotenv-rails"
--- spec/features/picture_post_spec.rb
+++ spec/features/picture_post_spec.rb
@@ -9,7 +9,6 @@ RSpec.feature "Pictures" do
     click_button "Upload Picture"
     page.attach_file("picture_image", Rails.root + "app/assets/images/another_cat.jpeg")
     click_button "Post"
-    click_button "Profile"
     expect(page).to have_css("img[src*='another_cat.jpeg']")
   end
 
@@ -19,7 +18,6 @@ RSpec.feature "Pictures" do
     click_button "Upload Picture"
     page.attach_file("picture_image", Rails.root + "app/assets/images/another_cat.jpeg")
     click_button "Post"
-    click_button "Profile"
     click_button "Delete"
     expect(page).not_to have_css("img[src*='another_cat.jpeg']")
   end
