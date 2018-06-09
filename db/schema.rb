7c095a416b7bd187b9ae32b8615aaaa3b2db1afa
Tom Betts <tom.betts@live.co.uk>
2018-06-09 14:32:17 +0100

    Add body column to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_09_111440) do
+ActiveRecord::Schema.define(version: 2018_06_09_132815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2018_06_09_111440) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.bigint "picture_id"
+    t.string "body"
     t.index ["picture_id"], name: "index_comments_on_picture_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
