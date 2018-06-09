99783a4f0155f00149a0f0b6e7ece239258ae5b3
Tom Betts <tom.betts@live.co.uk>
2018-06-09 19:24:03 +0100

    Add 'large' style to 'models/picture.rb'.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,7 +5,8 @@ class Picture < ApplicationRecord
   has_attached_file :image, styles: {
     thumb: "100x100>",
     square: "200x200#",
-    medium: "300x200#"
+    medium: "300x200#",
+    large: "500x340#"
   }
 
   # Validate the attached image is image/jpg, image/png, etc
