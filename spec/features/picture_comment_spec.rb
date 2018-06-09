c47d6271ccb4fa94131fc143d8d8ee89bc2d9687
Tom Betts <tom.betts@live.co.uk>
2018-06-10 00:04:44 +0100

    Write test for deleting comments.

--- spec/features/picture_comment_spec.rb
+++ spec/features/picture_comment_spec.rb
@@ -14,4 +14,18 @@ RSpec.feature "Picture Comments" do
     click_button "Comment"
     expect(page).to have_content("Nice Picture!")
   end
+
+  scenario "Can delete a comment" do
+      sign_up
+      click_button "Profile"
+      click_button "Upload Picture"
+      page.attach_file("picture_image", Rails.root + "app/assets/images/another_cat.jpeg")
+      click_button "Post"
+      click_button "Comment"
+      fill_in "comment[body]", with: "Nice Picture!"
+      click_button "Comment"
+      find(:css, 'img[src*="another_cat.jpeg"]').click
+      click_button "Delete"
+      expect(page).not_to have_content("Nice Picture!")
+  end
 end
