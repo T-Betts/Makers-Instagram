624caa2aec3685310d83986d53cfc4c1f2383d08
Tom Betts <tom.betts@live.co.uk>
2018-06-09 15:01:10 +0100

    Add author column to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,11 @@ class CommentsController < ApplicationController
   def new
     @picture = Picture.find(params[:picture_id])
   end
+
+  def create
+    @comment = @picture.comments.create(
+      comment_params.merge(commenter: current_user.username, user_id: current_user.id)
+    )
+    redirect_to post_path(@post)
+  end
 end
