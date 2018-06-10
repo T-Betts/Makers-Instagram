ba60e89433893ac032be66a3c6e6aa3e3936a6f4
Tom Betts <tom.betts@live.co.uk>
2018-06-10 11:31:40 +0100

    Refactor feautre tests.

--- spec/features/picture_post_spec.rb
+++ spec/features/picture_post_spec.rb
@@ -5,19 +5,13 @@ require "rails_helper"
 RSpec.feature "Pictures" do
   scenario "Can upload pictures." do
     sign_up
-    click_button "Profile"
-    click_button "Upload Picture"
-    page.attach_file("picture_image", Rails.root + "app/assets/images/another_cat.jpeg")
-    click_button "Post"
+    post_picture
     expect(page).to have_css("img[src*='another_cat.jpeg']")
   end
 
   scenario "Can delete pictures." do
     sign_up
-    click_button "Profile"
-    click_button "Upload Picture"
-    page.attach_file("picture_image", Rails.root + "app/assets/images/another_cat.jpeg")
-    click_button "Post"
+    post_picture
     click_button "Delete"
     expect(page).not_to have_css("img[src*='another_cat.jpeg']")
   end
